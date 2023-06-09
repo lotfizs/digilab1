9577d16ed092dca8edb8bffde15ed1f9f28767c9
lotfizs1 <lzsnation@hotmail.com>
2023-06-09 11:13:32 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+<section class="footer">
+
+   <div class="box-container">
+
+
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Lotfi Zaher Saidi</span> </p>
+
+</section>
---
+++ home.php
@@ -0,0 +1,106 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+if(isset($_POST['add_to_cart'])){
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_POST['product_image'];
+   $product_quantity = $_POST['product_quantity'];
+
+   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+   if(mysqli_num_rows($check_cart_numbers) > 0){
+      $message[] = 'already added to cart!';
+   }else{
+      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+      $message[] = 'product added to cart!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="home">
+
+   <div class="content">
+      <h3>Rondetijd applicatie</h3>
+      <p>Vergelijk jouw rondetijd met echte Formule 1 rondetijd</p>
+      <a href="about.php" class="white-btn">meer ontdekken</a>
+   </div>
+
+</section>
+
+<section class="products">
+
+   <h1 class="title">rondetijd invoegen</h1>
+
+
+   <div class="load-more" style="margin-top: 2rem; text-align:center">
+      <a href="shop.php" class="option-btn">invoegen/vergelijken</a>
+   </div>
+
+</section>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about-img.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Over ons</h3>
+         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quos enim minima ipsa dicta officia corporis ratione saepe sed adipisci?</p>
+         <a href="about.php" class="btn">meer lezen</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="home-contact">
+
+
+</section>
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
